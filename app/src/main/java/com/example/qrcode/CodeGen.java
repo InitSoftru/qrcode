b085978ca78306e6daf50340ea3a14b37e2bec11
InitSoftru <info@gorshkov-ev.ru>
2020-05-07 00:28:34 +0300

    Observable OK

--- app/src/main/java/com/example/qrcode/CodeGen.java
+++ app/src/main/java/com/example/qrcode/CodeGen.java
@@ -1,13 +1,14 @@
 package com.example.qrcode;
 //import android.databinding.ObservableBoolean;
-//import android.databinding.ObservableField;
+
+import androidx.databinding.ObservableField;
 public class CodeGen {
- //public ObservableField<String> code = new ObservableField <>();
-private String mCode;
-public CodeGen (String code)
+ public ObservableField<String> code = new ObservableField <>();
+//private String mCode;
+/*public CodeGen (String code)
 {
  mCode = code;
-}
-public String getCode(){return mCode;}
-public void setCode(String code){mCode=code;}
+}*/
+//public String getCode(){return mCode;}
+//public void setCode(String code){mCode=code;}
 }
--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -8,13 +8,16 @@ import android.os.Bundle;
 import com.example.qrcode.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-private CodeGen mCodeGen= new CodeGen("ddd");
+//private CodeGen mCodeGen= new CodeGen("ddd");
+private CodeGen mCodeGen= new CodeGen();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
+        mCodeGen.code.set("QWEW");
         binding.setCodegen(mCodeGen);
+        //binding.setCodegen(mCodeGen);
+
     }
 }
