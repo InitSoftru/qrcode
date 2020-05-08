2d70450af80259a92edc4f1c0aeda0520ed26a45
InitSoftru <info@gorshkov-ev.ru>
2020-05-09 01:30:14 +0300

    qr tost work

--- app/src/main/java/com/example/qrcode/CodeGen.java
+++ app/src/main/java/com/example/qrcode/CodeGen.java
@@ -1,8 +1,31 @@
 package com.example.qrcode;
-//import android.databinding.ObservableBoolean;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.databinding.ObservableBoolean;
+import android.view.View;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.PendingIntent;
+import android.graphics.Bitmap;
+import android.widget.Toast;
+import android.content.Context;
+import android.view.View;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+ //import static java.security.AccessController.getContext;
+//import static android.app.PendingIntent.getActivity;
 import androidx.databinding.ObservableField;
-public class CodeGen {
+public class CodeGen extends AppCompatActivity {
+
  public ObservableField<String> code = new ObservableField <>();
 //private String mCode;
 /*public CodeGen (String code)
@@ -11,4 +34,19 @@ public class CodeGen {
 }*/
 //public String getCode(){return mCode;}
 //public void setCode(String code){mCode=code;}
+private Context ctx;
+    public CodeGen(Context ctx){
+        this.ctx = ctx;
+    }
+ public void GenerateQR (View v)
+ {
+  Integer i;
+  String s =  code.get();
+  i=s.length();
+
+  if(i == 0){
+
+      Toast.makeText(ctx, "Введите код.", Toast.LENGTH_SHORT).show();
+  }
+  }
 }
--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -3,21 +3,43 @@ package com.example.qrcode;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.widget.Toast;
+import android.view.View;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
 import android.os.Bundle;
 
 import com.example.qrcode.databinding.ActivityMainBinding;
 
+import static android.app.PendingIntent.getActivity;
+
+//import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
 //private CodeGen mCodeGen= new CodeGen("ddd");
-private CodeGen mCodeGen= new CodeGen();
+private CodeGen mCodeGen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mCodeGen = new CodeGen(getApplicationContext());
         mCodeGen.code.set("QWEW");
         binding.setCodegen(mCodeGen);
         //binding.setCodegen(mCodeGen);
 
     }
+
 }
