c78088ed181b4d2bcf5e143b02f90090db16442e
InitSoftru <info@gorshkov-ev.ru>
2020-05-18 02:04:21 +0300

    Toolbar is work

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.qrcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.view.Menu;
 import android.widget.Toast;
 import android.view.View;
 import com.google.zxing.BarcodeFormat;
@@ -29,18 +33,41 @@ import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
 //private CodeGen mCodeGen= new CodeGen("ddd");
-
+private Toolbar toolbar;
 private CodeGen mCodeGen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+
+       // getSupportActionBar().setTitle("@string/menu_hystory");
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mCodeGen = new CodeGen(getApplicationContext());
         mCodeGen.code.set("QWEW");
         binding.setCodegen(mCodeGen);
+        //binding.toolbar.setTitle("@string/menu_hystory");
+        setSupportActionBar(binding.toolbar);
+        if (getSupportActionBar()==null) {
+
+            Toast.makeText(MainActivity.this, "Ошибка.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Не НОЛЬ!!!.", Toast.LENGTH_SHORT).show();
+
+        }
         //binding.setCodegen(mCodeGen);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return true;
+    }
+
 
 }
